c98ff4bc42f2456e0ae961df8a4f9bede691882a
otigon <73318661+otigon@users.noreply.github.com>
2021-04-14 10:55:48 -0500

    Update midi-handler.js

--- src/system-handlers/midi-handler.js
+++ src/system-handlers/midi-handler.js
@@ -76,8 +76,8 @@ export default class MidiHandler {
         //console.log(this._animNameFinal);
         this._animColorEffect;
         switch (true) {
-            case (this._animColor === "a1" || this._animColor === ``):
-            case !this._animColor:
+            case (this._animColor === ``):
+            case (!this._animColor):
                 this._animColorEffect = this._itemSource;
                 break;
             default:
