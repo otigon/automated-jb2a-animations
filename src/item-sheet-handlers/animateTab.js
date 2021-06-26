a2fd545210a51a38c15c51fa29d2a504536cdd43
otigon <xotigonx@hotmail.com>
2021-06-25 21:25:21 -0500

    working formApp

--- src/item-sheet-handlers/animateTab.js
+++ src/item-sheet-handlers/animateTab.js
@@ -1,34 +1,55 @@
-export class MyFormApplication extends FormApplication {
-    constructor(exampleOption) {
-      super();
-      this.exampleOption = exampleOption;
+import { AUTOANIM } from "./config.js";
+
+export class AAFormApplication extends FormApplication {
+    constructor() {
+        super(...arguments);
+        this.flags = this.object.data.flags.autoanimations
+        //this.exampleOption = exampleOption;
     }
-  
+
     static get defaultOptions() {
-      return mergeObject(super.defaultOptions, {
-        classes: ['form'],
-        popOut: true,
-        template: './modules/autoanimations/src/item-sheet-handlers/my-templates/myFormApplication.html',
-        id: 'my-form-application',
-        title: 'My FormApplication',
-      });
+        return mergeObject(super.defaultOptions, {
+            classes: ['form'],
+            popOut: true,
+            template: './modules/autoanimations/src/item-sheet-handlers/my-templates/myFormApplication.html',
+            id: 'AA-form-application',
+            title: 'AA FormApplication',
+            resizable: true,
+            width: 600,
+            closeOnSubmit: false
+        });
     }
-  
+
     getData() {
-      // Send data to the template
-      return {
-        msg: this.exampleOption,
-        color: 'red',
-      };
+        // Send data to the template
+        return {
+          msg: this.exampleOption,
+          color: 'red',
+          animationType: AUTOANIM.localized(AUTOANIM.animTypePick),
+          animationName: AUTOANIM.localized(AUTOANIM.animNameMeleeWeapon),
+          animationColor: AUTOANIM.localized(AUTOANIM.animColorMelee),
+          flags: this.object.data.flags,
+        };
+
     }
-  
+
     activateListeners(html) {
-      super.activateListeners(html);
+        super.activateListeners(html);
     }
-  
+
     async _updateObject(event, formData) {
-      console.log(formData.exampleInput);
+        console.log(formData);
+        formData = expandObject(formData);
+        if (!formData.changes)
+            formData.changes = [];
+        formData.changes = Object.values(formData.changes);
+        for (let c of formData.changes) {
+            //@ts-ignore
+            if (Number.isNumeric(c.value))
+                c.value = parseFloat(c.value);
+        }
+        return this.object.update(formData);
     }
-  }
-  
-  export default MyFormApplication;
+}
+
+export default AAFormApplication;
