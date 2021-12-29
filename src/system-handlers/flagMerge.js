28f149ef4f2565404b36ef3e6866d5b716437f29
otigon <73318661+otigon@users.noreply.github.com>
2021-12-29 16:15:23 -0600

    Add files via upload

--- src/system-handlers/flagMerge.js
+++ src/system-handlers/flagMerge.js
@@ -418,5 +418,40 @@ export const flagMigrations = {
 
             return data;
         },
+        "2": async (item) => {
+            /**
+             * allSounds.item.enableAudio --------------> audio.a01.enable
+             * allSounds.item.file ---------------------> audio.a01.file
+             * allSounds.item.delay --------------------> audio.a01.delay
+             * allSounds.item.volume -------------------> audio.a01.volume
+             * 
+             * allSounds.explosion.audioExplodeEnabled -> audio.e01.enable
+             * allSounds.explosion.file ----------------> audio.e01.file
+             * allSounds.explosion.delay ---------------> audio.e01.delay
+             * allSounds.explosion.volume --------------> audio.e01.volume
+             * 
+             * Subtracting 1 second from Explosion, Explosion Sound, and TargetFX delays
+             */
+            const v2Flags = item.data?.flags?.autoanimations || {};
+            const allSounds = v2Flags.allSounds || {};
+            v2Flags.audio = {
+                a01: {
+                    enable: allSounds.item?.enableAudio ?? false,
+                    file: allSounds.item?.file ?? "",
+                    delay: allSounds.item?.delay ?? 0,
+                    volume: allSounds.item?.volume ?? 0.25,
+                },
+                e01: {
+                    enable: allSounds.explosion?.audioExplodeEnabled || false,
+                    file: allSounds.explosion?.file ?? "",
+                    delay: allSounds.explosion?.delay ?? 0,
+                    volume: allSounds.explosion?.volume ?? 0.25,
+                }
+            }
+            v2Flags.version = 2;
+
+            await item.update({ 'flags.-=autoanimations': null })
+            await item.update({ 'flags.autoanimations': v2Flags })
+        }
     }
 }
