53621be1788a27927f2d1fb77d9bb8b0d00698a8
otigon <xotigonx@hotmail.com>
2022-01-11 19:56:49 -0600

    merge functions complete

--- src/custom-recognition/autoRecMerge.js
+++ src/custom-recognition/autoRecMerge.js
@@ -91,6 +91,7 @@ export const autoRecMigration = {
                         presetObject[i].custom02 = presetObject[i].custom ? true : false;
                         presetObject[i].customPath02 = presetObject[i].custom ? presetObject[i].customPath : "";
                         presetObject[i].subAnimation02 = presetObject[i].subAnimation ?? "mistystep";
+                        presetObject[i].variant = "01";
                         presetObject[i].variant02 = "02";
                         presetObject[i].color02 = presetObject[i].color ?? "blue";
                         presetObject[i].scale02 = presetObject[i].scale || 1;
