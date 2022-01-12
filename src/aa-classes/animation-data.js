4fb1cb06d2d6162f84f7a2dcee8002ae53dba606
otigon <xotigonx@hotmail.com>
2022-01-11 19:41:54 -0600

    autorec merge start

--- src/aa-classes/animation-data.js
+++ src/aa-classes/animation-data.js
@@ -28,7 +28,7 @@ export class AAanimationData {
             data.color = autoOverridden ? handler.autoOverride?.color : data.color;
             data.color02 = autoOverridden ? handler.autoOverride?.color02 : data.color02;
             data.repeat = autoOverridden ? handler.autoOverride?.repeat : data.repeat;
-            data.delay = autoOverridden ? handler.autoOverride?.delay : data.delay;
+            data.delay = autoOverridden ? handler.autoOverride?.delay : data.delay || 0;
             data.scale = autoOverridden ? handler.autoOverride?.scale || 1 : data.scale || 1;
             data.scale02 = autoOverridden ? handler.autoOverride?.scale02 || 1 : data.scale02 || 1;
             data.scaleX = autoOverridden ? handler.autoOverride?.scaleX || 1 : data.scaleX || 1;
--- src/custom-recognition/autoRecMerge.js
+++ src/custom-recognition/autoRecMerge.js
@@ -72,6 +72,32 @@ export const autoRecMigration = {
             }
             currentAutorec.version = 2;
             await game.settings.set('autoanimations', 'aaAutorec', currentAutorec)
+        },
+        "3": async (currentAutorec) => {
+            const templateObject = currentAutorec.templates;
+            if (templateObject) {
+                const templateLength = Object.keys(templateObject).length;
+                for (var i = 0; i < templateLength; i++) {
+                    templateObject[i].scaleX = templateObject[i].scale || 1;
+                    templateObject[i].scaleY = templateObject[i].scale || 1;
+                }
+            }
+            const presetObject = currentAutorec.preset;
+            if (presetObject) {
+                const presetLength = Object.keys(presetObject).length;
+                for (var i = 0; i < presetLength; i++) {
+                    if (presetObject[i].animation === "teleportation") {
+                        presetObject[i].measureType = "alternating";
+                        presetObject[i].custom02 = presetObject[i].custom ? true : false;
+                        presetObject[i].customPath02 = presetObject[i].custom ? presetObject[i].customPath : "";
+                        presetObject[i].subAnimation02 = presetObject[i].subAnimation ?? "mistystep";
+                        presetObject[i].variant02 = "02";
+                        presetObject[i].color02 = presetObject[i].color ?? "blue";
+                        presetObject[i].scale02 = presetObject[i].scale || 1;
+                    }
+                }
+            }
+            currentAutorec.version = 3;
         }
     }
 }
