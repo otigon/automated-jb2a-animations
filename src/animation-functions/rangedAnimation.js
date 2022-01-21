bae3d005aace454876f441776bec40e8e756c0e7
otigon <xotigonx@hotmail.com>
2022-01-20 18:04:02 -0600

    updates

--- src/animation-functions/rangedAnimation.js
+++ src/animation-functions/rangedAnimation.js
@@ -38,13 +38,69 @@ export async function rangedAnimations(handler, animationData) {
             } else {
                 hit = false;
             }
+            /*
             if (data.macro.enabled && data.macro.name) {
                 let executeMacro = new Sequence("Automated Animations");
                 let userData = data.macro.args.split(',').map(s => s.trim());
                 executeMacro.macro(data.macro.name, {sourceToken, target, animationData, handler}, ...userData)
                 executeMacro.play()
             }
-            
+            */
+            let rs = await new Sequence("Automated Animations")
+            if (sourceFX.enabled) {
+                rs.addSequence(sourceFX.sourceSeq)
+            }
+            rs.thenDo(function() {
+                Hooks.callAll("aa.animationStart", sourceToken, target)
+            })
+            if (data.playMacro && data.macro.playWhen === 'primary') {
+                let userData = data.macro.args;
+                rs.macro(data.macro.name, {sourceToken, target, animationData, handler}, ...userData)
+            }
+            if (data.itemAudio.enable){
+                rs.sound()
+                    .file(data.itemAudio.file, true)
+                    .volume(data.itemAudio.volume)
+                    .delay(data.itemAudio.delay)
+                    .repeats(data.itemAudio.repeat, data.delay)
+                    .playIf(data.playSound)
+            }
+            rs.effect()
+                .file(attack.file)
+                .atLocation(sourceToken)
+                .stretchTo(target, {onlyX: onlyX})
+                .randomizeMirrorY()
+                .repeats(data.repeat, data.delay)
+                .missed(hit)
+                .name("animation")
+                .belowTokens(data.below)
+            if (data.explosion.enabled) {
+                if (data.explosion.audio.enabled) {
+                    rs.sound()
+                        .file(data.explosion?.audio?.file, true)
+                        .playIf(data.explosion?.playSound)
+                        .delay(data.explosion?.audio?.delay + data.explosion?.delay)
+                        .volume(data.explosion?.audio?.volume)
+                        .repeats(data.explosion?.audio?.repeat, data.delay)
+                }
+                rs.effect()
+                    .atLocation("animation")
+                    .file(data.explosion?.data?.file, true)
+                    .scale({ x: data.explosion?.scale, y: data.explosion?.scale })
+                    .delay(data.explosion?.delay)
+                    .repeats(data.repeat, data.delay)
+                    .belowTokens(data.explosion?.below)
+                    .playIf(data.explosion?.enabled)
+                if (data.playMacro && data.macro.playWhen === 'explosion') {
+                    let userData = data.macro.args;
+                    rs.macro(data.macro.name, {sourceToken, target, animationData, handler}, ...userData)
+                }        
+            }
+            if (targetFX.enabled) {
+                rs.addSequence(targetSequence.targetSeq)
+            }
+            rs.play()
+                /*
             await new Sequence("Automated Animations")
                 .addSequence(sourceFX.sourceSeq)
                 .thenDo(function() {
@@ -82,6 +138,7 @@ export async function rangedAnimations(handler, animationData) {
                     .playIf(data.explosion?.enabled)
                 .addSequence(targetSequence.targetSeq)
                 .play()
+                */
                 await wait(handler.animEnd)
                 Hooks.callAll("aa.animationEnd", sourceToken, target)
         }
