e055346c5b977dbd59109b572890849cc181265c
typhonrt <support@typhonrt.org>
2022-06-15 17:56:45 -0700

    Added constants

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+/**
+ * Defines the main constants for module name and label.
+ *
+ * @type {{moduleId: string, moduleLabel: string}}
+ */
+const constants = {
+   moduleId: "autoanimations",
+   moduleLabel: "Automated Animations"
+};
+
+export { constants };
