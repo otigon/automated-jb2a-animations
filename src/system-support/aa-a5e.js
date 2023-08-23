3082c2791ecfedd08660ea065970ddc6e29fe740
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-08-23 16:08:54 -0700

    Changes how template effects are handled.

--- src/system-support/aa-a5e.js
+++ src/system-support/aa-a5e.js
@@ -3,37 +3,53 @@ import AAHandler            from "../system-handlers/workflow-data.js";
 import { getRequiredData }  from "./getRequiredData.js";
 
 export function systemHooks() {
-    Hooks.on("createChatMessage", async (msg) => {
-        if (msg.user.id !== game.user.id) { return };
+  // Create queue to handle interaction between template and item.
+  const queue = [];
 
-        const item = await fromUuid(msg.flags?.a5e?.itemId);
+  Hooks.on("createChatMessage", async (msg) => {
+    if (msg.user.id !== game.user.id) { return };
 
-        const compiledData = await getRequiredData({
-            item: item,  
-            itemUuid: msg.flags?.a5e?.itemId,
-            actorId: msg.speaker?.actorId,
-            tokenId: msg.speaker?.token,
-            workflow: msg,
-        });
+    const item = await fromUuid(msg.flags?.a5e?.itemId);
 
-        runA5e(compiledData);
+    // Get from queue
+    let templateData = [];
+    while (queue.length && queue[0][0] === item.uuid) templateData.push(queue.shift()[1]);
+
+    if (templateData.length) {
+      templateData.forEach((data) => runA5e(data));
+      return;
+    }
+
+    const compiledData = await getRequiredData({
+      item: item,  
+      itemUuid: msg.flags?.a5e?.itemId,
+      actorId: msg.speaker?.actorId,
+      tokenId: msg.speaker?.token,
+      workflow: msg,
     });
 
-    Hooks.on('a5e.measuredTemplatePlaced', async (item, templateData, userId) => {
-        if (userId !== game.user.id) return;
+    runA5e(compiledData);
+  });
 
-        const compiledData = await getRequiredData({
-            item,
-            templateData: templateData,
-            workflow: templateData,
-            isTemplate: true,
-        })
+  Hooks.on('a5e.measuredTemplatePlaced', async (item, templateData, userId) => {
+    if (userId !== game.user.id) return;
+    if (templateData.length) templateData = templateData[0];
+    else return;
 
-        runA5e(compiledData);
+    const compiledData = await getRequiredData({
+      item,
+      templateData: templateData,
+      workflow: templateData,
+      isTemplate: true,
     });
+
+    // Add to queue
+    queue.push([item.uuid, compiledData]);
+  });
 }
 
 async function runA5e(input) {
-    const handler = await AAHandler.make(input);
-    trafficCop(handler);
+  const handler = await AAHandler.make(input);
+  if (!handler?.item || !handler?.sourceToken) return;
+  trafficCop(handler);
 }
