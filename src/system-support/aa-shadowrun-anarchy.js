4473b6364d29958a5f4b34f102f31f2e07e4a776
ralke1 <caiosmenezes@outlook.com>
2023-08-27 09:52:51 -0300

    shadowrun anarchy working for manual setted automatic animations on global and item menus

--- src/system-support/aa-shadowrun-anarchy.js
+++ src/system-support/aa-shadowrun-anarchy.js
@@ -1,70 +1,86 @@
-import { AnimationState } from "../AnimationState.js";
-import { debug } from "../constants/constants.js";
-import { trafficCop } from "../router/traffic-cop.js";
-import AAHandler from "../system-handlers/workflow-data.js";
-import { getRequiredData } from "./getRequiredData.js";
-
+import { AnimationState }   from "../AnimationState.js";
+import { debug }            from "../constants/constants.js";
+import { trafficCop }       from "../router/traffic-cop.js";
+import AAHandler            from "../system-handlers/workflow-data.js";
+import { getRequiredData }  from "./getRequiredData.js";
 
 export function systemHooks() {
-    Hooks.on("createChatMessage", async (msg) => {checkChatMessage(msg) });
+    Hooks.on("renderChatMessage", async (msg) => {
+        let rawDataMsg = msg.getFlag('anarchy','message-data');
+        if (!rawDataMsg){
+            return;
+        }
+        const anarchyData = JSON.parse(msg.getFlag('anarchy','message-data'));
+        checkChatMessageAnarchy(msg, anarchyData);
+    });
 }
 
-const TEST = {
-    Melee: 'MeleeAttackTest',
-    Thrown: 'ThrownAttackTest',
-    Range: 'RangedAttackTest',
-    Defense: 'PhysicalDefenseTest',
-    Spell: 'SpellCastingTest',
-    Drain: 'DrainTest',
-    Skill: 'SkillTest',
-}
-
-
-function extractItemId(content) {
-    try {
-        return JSON.parse(msg.flags.anarchy['message-data']);
-    } catch (exception) {
-        console.log("COULD NOT GET ITEM ID")
-        return null;
-    }
-};
-
-async function checkChatMessage(msg) {
-    if (msg.user.id !== game.user.id || !AnimationState.enabled) {
+async function checkChatMessageAnarchy(msg, anarchyData) {
+    if (msg.user.id !== game.user.id || !AnimationState.enabled) { 
         return;
     }
-    //msg.flags.anarchy['message-data']
+
+    const compiledData = await computeCompiledDataAnarchy(msg, anarchyData);
+    if (!compiledData) { 
+        return;
+    }
+
+    if (await tryAnnimationWith(compiledData)) {
+        return;
+    }
+
+    // Depending on the type of the attack (e.g., ranged, melee), you can add more cases
+    switch (anarchyData.attackRoll.mode) {
+        case "weapon":
+            const weaponName = anarchyData.attackRoll.weapon.id; // Assuming the weapon id is a good fallback
+            if (weaponName && await tryAnnimationWith(compiledData, weaponName)) {
+                return;
+            }
+            break;
+        // Add cases for other modes if needed
+    }
+
+    // If no specific mode matches, try using the skill name for animation
+    const skill = anarchyData.attackRoll.skill.id;
+    if (skill && await tryAnnimationWith(compiledData, skill)) {
+        return;
+    }
+}
+
+async function computeCompiledDataAnarchy(msg, anarchyData) {
+    let actor = game.actors.get(anarchyData?.attackRoll?.actor?.id);
+    debug('Anarchy actor', actor);
+    let weaponId = anarchyData?.attackRoll?.weapon.id;
+    let item = {
+        id: weaponId,
+        name: actor?.items?.get(weaponId)?.name,
+    };
     
-    messageData = extractItemId(msg);
-    let actorId =messageData.targeting.attackerTokenId;              // getting actor id
-    let targets = Array.from(messageData.targeting.targetedTokenIds); // getting targets array
-    let weaponId = messageData.weapon.id;            // getting item that is a weapon id
+    debug('Anarchy content to  getRequiredData:', {
+        item: item,
+        actorId: actor,
+    });
+    const compiledData = await getRequiredData({
+        item: item,
+        actorId: msg.speaker?.actor,
+        workflow: msg,
+    });
 
-
-    let compiledData = await getRequiredData({
-        item: weaponId,
-        actor: actorId,
-        targets: targets,
-        workflow: weaponId,        
-    })
-
-    //5e example
-    //await getRequiredData({item, actor: item.actor, workflow: item, rollAttackHook: {item, roll}, 
-        //spellLevel})
-
-    // Assuming Shadowrun Anarchy system uses a unique identifier like 'anarchyRoll'
-    /*const roll = msg.flags?.anarchy?.anarchyRoll;
-    if (!roll) {
-        return;
-    }*/
-
-    if (!compiledData.item) { return; }
-    runShadowrunAnarchy(compiledData)
-
+    return compiledData;
 }
 
-// Export this module for other scripts to use
-async function runShadowrunAnarchy(input) {
-    const handler = await AAHandler.make(input)
-    trafficCop(handler);
+async function tryAnnimationWith(compiledData, itemNameOverride) {
+    if (itemNameOverride) {
+        compiledData.item = {
+            name: itemNameOverride
+        }
+    }
+
+    const handler = await AAHandler.make(compiledData);
+    if (handler?.item && handler.sourceToken) { 
+        trafficCop(handler);
+        return true;
+    }
+
+    return false;
 }
