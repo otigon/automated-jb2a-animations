1e99f04a7ceb8b5103eb8d13633bc564caa60157
Marek Kembrowski <marek.kembrowski@se.gt.com>
2024-01-06 00:29:08 +0100

    merge

--- src/system-support/index.js
+++ src/system-support/index.js
@@ -28,4 +28,4 @@ export * as earthdawn4e from "./aa-ed4e.js"
 export * as ptu from "./aa-ptu.js";
 export * as lancer from "./aa-lancer.js";
 export * as anarchy from "./aa-shadowrun-anarchy.js";
-
+export * as wrathandglory from "./aa-wrath-and-glory.js";
