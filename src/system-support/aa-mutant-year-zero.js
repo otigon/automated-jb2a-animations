859f514c89355467f051b3caad3b55a8bbdb29dc
Coyotito007 <45643619+Coyotito007@users.noreply.github.com>
2024-04-08 09:08:59 +0200

    Create aa-mutant-year-zero.js

---
+++ src/system-support/aa-mutant-year-zero.js
@@ -0,0 +1,32 @@
+import { trafficCop }       from "../router/traffic-cop.js"
+import AAHandler            from "../system-handlers/workflow-data.js";
+import { getRequiredData }  from "./getRequiredData.js";
+// WILL NEED REWORK AFTER THE V10 VERSION IS RELEASED
+export function systemHooks() {
+    Hooks.on("createChatMessage", async (msg) => {
+        if (msg.user.id !== game.user.id) { return };
+
+        function extractItemId(content) {
+            try {
+                return $(content).attr("data-item-id");
+            } catch (exception) {
+                console.log("COULD NOT GET ITEM ID")
+                return null;
+            }
+        }
+
+        let compiledData = await getRequiredData({
+            itemId: extractItemId(msg.content),
+            actorId: msg.speaker?.actor,
+            tokenId: msg.speaker?.token,
+            workflow: msg,
+        })
+        if (!compiledData.item) { return; }
+        runmutantyearzero(compiledData)
+    });
+}
+
+async function runmutantyearzero(input) {
+    const handler = await AAHandler.make(input)
+    trafficCop(handler);
+}
